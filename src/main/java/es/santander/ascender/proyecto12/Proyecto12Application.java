db1eb6410afe2e77c7fcc2d16c639126251f6920
nicolas <nico@gmail.com>
2024-12-03 19:16:23 +0100

    primer commit

---
+++ src/main/java/es/santander/ascender/proyecto12/Proyecto12Application.java
@@ -0,0 +1,13 @@
+package es.santander.ascender.proyecto12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto12Application.class, args);
+	}
+
+}
